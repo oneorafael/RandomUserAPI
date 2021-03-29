e319d4dbffce86c0f2027dd85156f65e679d4aa6
Rafael Oliveira <oliveirafael@icloud.com>
2021-03-29 08:25:40 -0300

    - create a base User Model;

---
+++ RandomUserRestApi/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  RandomUserRestApi
+//
+//  Created by Rafael Oliveira on 29/03/21.
+//  Copyright © 2021 Rafael Oliveira. All rights reserved.
+//
+
+import Foundation
+struct User {
+    let name: String
+    let email: String
+    let Birthday: String
+}
