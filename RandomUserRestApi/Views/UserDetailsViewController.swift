68a823c689ba68c734101786deddd973c2bae23e
Rafael Oliveira <oliveirafael@icloud.com>
2021-10-25 13:58:42 -0300

    create cardViewDetail (dummie data yet) WIP

--- RandomUserRestApi/Views/UserDetailsViewController.swift
+++ RandomUserRestApi/Views/UserDetailsViewController.swift
@@ -21,7 +21,57 @@ class UserDetailsViewController:UIViewController {
         closeBtn.addTarget(self, action: #selector(dismissWindow), for: .touchUpInside)
         return closeBtn
     }()
+    private lazy var PersonImage: UIImageView = {
+        let personIMG = UIImage(named: "edgar")
+        let PersonImage = UIImageView()
+        PersonImage.image = personIMG
+        PersonImage.contentMode = .scaleAspectFit
+        PersonImage.translatesAutoresizingMaskIntoConstraints = false
+//        PersonImage.layer.cornerRadius = 50
+//        PersonImage.clipsToBounds = true
+        return PersonImage
+    }()
     
+    private lazy var titleLabel:UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "Edgar"
+        titleLabel.font = UIFont.systemFont(ofSize: 33, weight: .black)
+        titleLabel.textColor = .gray
+        return titleLabel
+    }()
+    private lazy var birthdayLabel:UILabel = {
+        let birthdayLabel = UILabel()
+        birthdayLabel.text = "data de nascimento"
+        birthdayLabel.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        birthdayLabel.textColor = .gray
+        return birthdayLabel
+    }()
+    private lazy var emailLabel:UILabel = {
+        let emailLabel = UILabel()
+        emailLabel.text = "Email"
+        emailLabel.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        emailLabel.textColor = .gray
+        return emailLabel
+    }()
+    private lazy var phoneLabel:UILabel = {
+        let phoneLabel = UILabel()
+        phoneLabel.text = "Telefone"
+        phoneLabel.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        phoneLabel.textColor = .gray
+        return phoneLabel
+    }()
+    
+    //StackView
+    private lazy var verticalStack:UIStackView = {
+        let verticalStack = UIStackView()
+        verticalStack.axis = .vertical
+        verticalStack.addArrangedSubview(titleLabel)
+        verticalStack.addArrangedSubview(birthdayLabel)
+        verticalStack.addArrangedSubview(emailLabel)
+        verticalStack.addArrangedSubview(phoneLabel)
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        return verticalStack
+    }()
     @objc func dismissWindow(){
         self.dismiss(animated: true, completion: nil)
     }
@@ -33,11 +83,19 @@ class UserDetailsViewController:UIViewController {
     
     private func  setupViews() {
         view.addSubview(closeBtn)
+        view.addSubview(PersonImage)
+        view.addSubview(verticalStack)
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
             closeBtn.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 16),
             closeBtn.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
-            closeBtn.heightAnchor.constraint(equalToConstant: 50)
+            closeBtn.heightAnchor.constraint(equalToConstant: 50),
+            PersonImage.topAnchor.constraint(equalTo: closeBtn.bottomAnchor, constant: 10),
+            PersonImage.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            PersonImage.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            PersonImage.heightAnchor.constraint(equalToConstant: 350),
+            verticalStack.topAnchor.constraint(equalTo: PersonImage.bottomAnchor, constant: 30),
+            verticalStack.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
         ])
     }
 }
